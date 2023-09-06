a148506b73ec6236062da2dad8fe34d969a5b1ec
Mohamed Ben Noba <54261592+mohamed-benoba@users.noreply.github.com>
2023-09-06 18:04:25 +0200

    Comment all prev code, and make soon component

--- js/main.js
+++ js/main.js
@@ -1,81 +1,61 @@
-import * as dataModule from './data.js'
-import * as tabsModule from './tabs.js'
+// import * as dataModule from './data.js'
+// import * as tabsModule from './tabs.js'
 
-tabsModule.createTheAllTab();
-tabsModule.createTabs();
+// tabsModule.createTheAllTab();
+// tabsModule.createTabs();
 
-// document.querySelector('.reviews-link').addEventListener('click', () => {
-//   var dialog = document.querySelector('.overlay');
-//   dialog.style.display = 'block';
-// })
+// // Search
+// function search() {
+//   const searchInput = document.getElementById('searchInput').value.toLowerCase();
+//   const results = [];
+//   dataModule.subjects.forEach((title) => {
+//     const titleText = title.name.toLowerCase();
+//     if (titleText.replace(/[أإ]/g, 'ا').includes(searchInput.replace(/[أإ]/g, 'ا'))) {
+//       results.push(title.name);
+//     }
+//   });
 
-// document.querySelector('.overlay').addEventListener('click', () => {
-//   var dialog = document.querySelector('.overlay');
-//   dialog.style.display = 'none';
-// })
+//   displayResults(results, searchInput);
+// }
 
-// document.querySelector('.body').addEventListener('click', () => {
-//   var dialog = document.querySelector('.overlay');
-//   dialog.style.display = 'none';
-// })
+// function displayResults(results) {
+//   const searchResults = document.getElementById('searchResults');
+//   searchResults.innerHTML = '';
 
-// document.querySelector('.close-dialog').addEventListener('click', () => {
-//   var dialog = document.querySelector('.overlay');
-//   dialog.style.display = 'none';
-// })
+//   if (results.length === 0) {
+//     searchResults.innerHTML = '<ul>لا يوجد أي نتائج، تحقق من عدم وجود خطأ إملائي.</ul>';
+//     return;
+//   }
+//   if (searchInput.value.length === 0) {
+//     searchResults.innerHTML = '';
+//     return;
+//   }
 
-// Search
-function search() {
-  const searchInput = document.getElementById('searchInput').value.toLowerCase();
-  const results = [];
-  dataModule.subjects.forEach((title) => {
-    const titleText = title.name.toLowerCase();
-    if (titleText.replace(/[أإ]/g, 'ا').includes(searchInput.replace(/[أإ]/g, 'ا'))) {
-      results.push(title.name);
-    }
-  });
+//   const resultList = document.createElement('ul');
+//   results.forEach((result) => {
+//     const listItem = document.createElement('li');
+//     listItem.textContent = result;
+//     resultList.appendChild(listItem);
+//     listItem.addEventListener('click', filterBasedOnSearchClick)
+//   });
 
-  displayResults(results, searchInput);
-}
+//   searchResults.appendChild(resultList);
+// }
 
-function displayResults(results) {
-  const searchResults = document.getElementById('searchResults');
-  searchResults.innerHTML = '';
+// const tabs = document.querySelector('.tabs');
 
-  if (results.length === 0) {
-    searchResults.innerHTML = '<ul>لا يوجد أي نتائج، تحقق من عدم وجود خطأ إملائي.</ul>';
-    return;
-  }
-  if (searchInput.value.length === 0) {
-    searchResults.innerHTML = '';
-    return;
-  }
+// function filterBasedOnSearchClick() {
 
-  const resultList = document.createElement('ul');
-  results.forEach((result) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = result;
-    resultList.appendChild(listItem);
-    listItem.addEventListener('click', filterBasedOnSearchClick)
-  });
+//   const tab = document.querySelector(`.tabs-container .tab[name="${this.textContent}"]`);
+//   const searchInput = document.getElementById('searchInput');
+//   const searchResults = document.getElementById('searchResults');
+//   searchInput.value = tab.textContent;
+//   searchResults.textContent = ''
+//   // Programmatically trigger a real click event
+//   tab.click()
+// }
 
-  searchResults.appendChild(resultList);
-}
-
-const tabs = document.querySelector('.tabs');
-
-function filterBasedOnSearchClick() {
-
-  const tab = document.querySelector(`.tabs-container .tab[name="${this.textContent}"]`);
-  const searchInput = document.getElementById('searchInput');
-  const searchResults = document.getElementById('searchResults');
-  searchInput.value = tab.textContent;
-  searchResults.textContent = ''
-  // Programmatically trigger a real click event
-  tab.click()
-}
-
-const searchInput = document.getElementById('searchInput');
-searchInput.addEventListener('input', search);
+// const searchInput = document.getElementById('searchInput');
+// searchInput.addEventListener('input', search);
 
 
